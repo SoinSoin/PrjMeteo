0f3412d89612b015c59228af19444f06f3f45963
NicoSimplon <marty.nicolas11@laposte.net>
2017-12-13 16:06:02 +0100

    nouvelle version de la barre

--- alerte.js
+++ alerte.js
@@ -1,21 +1,11 @@
-// function alarme(boiteAlerte){
-// 	// if (document.getElementById('boiteAlerte').title='hidden'){
-// 	// 	document.getElementById('boiteAlerte').style.visibility='visible';
-// 	// 	document.getElementById('boiteAlerte').title='visible';
-// 	// }
-// 	// else if (document.getElementById('boiteAlerte').title='visible'){
-// 	// 	document.getElementById('boiteAlerte').style.visibility='hidden';
-// 	// 	document.getElementById('boiteAlerte').title='hidden';
-// 	// }
-// 	// else{
+function alerte(id){
+    var n=5
+if (n==5){
+    document.getElementById(id).style.visibility ='visible';
+    }
 
-// 	// }
-// }
+else {
+    document.getElementById(id).style.visibility='hidden';
+    }
+};
 
-function toggle_visibility(id){
-	var e = document.getElementById(id);
-	if(e.style.display == 'none')
-		e.style.display = 'block';
-	else
-		e.style.display = 'none';
-}
